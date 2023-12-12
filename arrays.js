947252ecead44babe5ce880a83ed5e7ff13d570e
HSYN <huseyn.mirzayev@code.edu.az>
2023-12-12 23:57:16 +0400

    finish task

---
+++ arrays.js
@@ -0,0 +1,48 @@
+const countries = [
+  "Pakistan",
+  "India",
+  "Tajikistan",
+  "Russia",
+  "Kazakhstan",
+  "Greece",
+  "Turkmenistan",
+  "Turkey",
+  "Kyrgyzstan",
+  "Ukraine",
+  "Uzbekistan",
+];
+
+const sentence = "In the end, we only regret the chances we didn't take.";
+
+const books = [
+  "To Kill a Mockingbird",
+  "1984",
+  "The Great Gatsby",
+  "Pride and Prejudice",
+  "The Catcher in the Rye",
+  "The Lord of the Rings",
+  "Animal Farm",
+  "Brave New World",
+  "The Grapes of Wrath",
+  "The Hobbit",
+  "One Hundred Years of Solitude",
+  "The Catcher in the Rye",
+  "Catch-22",
+  "The Brothers Karamazov",
+  "Moby Dick",
+  "The Odyssey",
+  "Frankenstein",
+  "Alice's Adventures in Wonderland",
+  "The Picture of Dorian Gray",
+  "War and Peace",
+];
+
+// 1. Countries array-ində sonu 'stan' ilə bitən ölkələrdən array yaratmaq
+
+// 2. Sentence dəyişənindəki bütün sözlərin baş hərfləri böyük şəkildə yazmaq. ("In The End" - bu formada)
+
+// 3. Sentence dəyişənini tərsinə yazmaq. (In the end, we only regret the chances we didn't take. - bu formada)
+
+// 4. Sentence dəyişənində olan ən uzun sözü tapmaq.
+
+// 5. 
